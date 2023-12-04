904c1eda0772bd552fc7a349f938add513a44a66
KaiserCarlos <carlos.vilarchao710@gmail.com>
2023-12-04 11:55:10 +0100

    lab hibernate

--- src/main/java/modelo/HibernateUtil.java
+++ src/main/java/modelo/HibernateUtil.java
@@ -4,7 +4,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-	
+	//yepa
 	private static final SessionFactory sessionFactory = buildSessionFactory();
 	private static SessionFactory buildSessionFactory() {
 	try {
